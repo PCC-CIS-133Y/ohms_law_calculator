4098108e4958bd547673d6abf18a9636fc85a7be
Marc Goodman <marc.goodman@pcc.edu>
2020-09-11 00:45:44 -0700

    Adding some comments.

--- calculator.py
+++ calculator.py
@@ -2,14 +2,20 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 class SimpleApp:
+    # Construct the user interface. There are two inputs for Volts and Ohms, and two buttons.
+    # One button clears the inputs and the other button calculates Amps and Watts based on
+    # Volts and Ohms.
     def __init__(self, parent):
-        # build ui
+        # build root frame of ui
         top_frame = ttk.Frame(parent, padding=10)
         top_frame.grid(column=0, row=0, sticky="nsew")
 
+        # Add a label to the top
         header_label = ttk.Label(top_frame, font="{Arial} 16 {bold}", text="Ohm's Law Calculator")
         header_label.grid(column=0, row=0, pady=3)
 
+        # The input boxes and labels are in a nested frame for convenience.
+        # This actually makes layout simpler.
         input_frame = ttk.Frame(top_frame, padding=10)
         input_frame.grid(column=0, row=1)
 
@@ -23,13 +29,16 @@ class SimpleApp:
         ohms_entry = ttk.Entry(input_frame)
         ohms_entry.grid(column=1, row=1, pady=3)
 
+        # The buttons are also in a nested frame for convenience.
         button_frame = ttk.Frame(top_frame)
         button_frame.grid(column=0, row=2, sticky="e")
         button_frame.rowconfigure(0, weight=1)
         button_frame.columnconfigure(0, weight=1)
 
+        # Clear button calls the clear method.
         clear_button = ttk.Button(button_frame, text="Clear", command=self.clear)
         clear_button.grid(column=0, row=0)
+        # OK button calls the calculate method.
         ok_button = ttk.Button(button_frame, text="OK", command=self.calculate)
         ok_button.grid(column=1, row=0)
 
@@ -38,6 +47,9 @@ class SimpleApp:
         self.volts_entry = volts_entry
         self.ohms_entry = ohms_entry
 
+    # Results are shown in a separate window.
+    # The answer window just has a label at the top and a multiline blob of text under the label.
+    # We use this window for both error messages and for the results of the calculation.
     def open_answer_frame(self, parent, text):
         answer_top_frame = ttk.Frame(parent, padding=10)
         answer_top_frame.grid(column=0, row=0, sticky="nsew")
@@ -47,11 +59,14 @@ class SimpleApp:
         answer_label = ttk.Label(answer_top_frame, text=text, justify=tk.CENTER)
         answer_label.grid(column=0, row=1, pady=5)
 
+    # Calculate Amps and Watts based on Volts and Ohms
     def calculate(self):
         print("Calculating.")
         # self.mainwindow = self.builder.get_object('output_frame')
         # self.builder.connect_callbacks(self)
         try:
+            # Fetch the values from the entry boxes and use them to calculate the results.
+            # If there was an error anywhere, tell the user that the inputs were invalid.
             volts = float(self.volts_entry.get())
             ohms = float(self.ohms_entry.get())
             amps = volts / ohms
@@ -63,12 +78,14 @@ class SimpleApp:
                                          + "and that ohms is greater than 0." )
             return
 
+        # The calculation worked, so let's show the results to the user.
         result_text = (str(volts) + " volts running through " + str(ohms) + " Ohms would be:\n\n"
                              + "{:.2f} Amps\n".format(amps)
                              + "{:.2f} Watts".format(watts))
         top2 = tk.Toplevel(self.mainwindow)
         self.open_answer_frame(top2, result_text)
 
+    # Clear the entry boxes
     def clear(self):
         self.volts_entry.delete(0, tk.END)
         self.ohms_entry.delete(0, tk.END)
